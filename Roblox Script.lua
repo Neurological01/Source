9e65190c93764f65081e822d469b31e44de28312
Neurological01 <113656521+Neurological01@users.noreply.github.com>
2022-09-24 19:47:22 +0800

    Add files via upload

---
+++ Roblox Script.lua
@@ -0,0 +1,8 @@
+--best script fr
+
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "DUMBASS LOL";
+Text = "You are stupid fr just search online LOL";
+Duration = 1000000000000000000;
+})
